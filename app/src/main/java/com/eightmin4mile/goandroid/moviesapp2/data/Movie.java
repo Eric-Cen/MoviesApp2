bc16be3f4785d830e05dc276e1c3b62cad0c9709
goAndroid <goandroid@OTPUser.local>
2018-06-06 16:52:52 -0400

    starting..

---
+++ app/src/main/java/com/eightmin4mile/goandroid/moviesapp2/data/Movie.java
@@ -0,0 +1,26 @@
+package com.eightmin4mile.goandroid.moviesapp2.data;
+
+import android.os.Parcelable;
+
+/**
+ * Created by goandroid on 6/1/18.
+ */
+
+public class Movie{
+
+    // the titles and IDs of the user's favorite movies
+    // to store movie poster, synopsis, user rating and release date
+
+    private String title;
+    private int id;
+
+    private String poster;
+    private String synopsis;
+    private int rating;
+    private String releaseDate;
+
+
+
+
+
+}
